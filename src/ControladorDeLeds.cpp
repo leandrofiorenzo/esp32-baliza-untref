ef129873b2b752715bfea366b155d33e1e46753d
Lucho20 <muratore2401@gmail.com>
2019-11-09 16:15:22 -0300

    Cambio de Estados de Led

--- src/ControladorDeLeds.cpp
+++ src/ControladorDeLeds.cpp
@@ -4,8 +4,9 @@
 void ControladorDeLeds::prenderLedCorrespondienteAlEstado(EstadoBuildEnum estadoBuild) {
     if(estadoBuild == EstadoBuildEnum::Creado) {
         Serial.println("Estoy Creado");
-    } else if(estadoBuild == EstadoBuildEnum::Desconocido) {
-        Serial.println("Estoy Desconocido");
+    } /*else if(estadoBuild == EstadoBuildEnum::Desconocido) {*/
+    else if(estadoBuild == EstadoBuildEnum::Desconexion) {
+        Serial.println("Estoy Desconectado de WiFi");
     } else if(estadoBuild == EstadoBuildEnum::EnCurso) {
         Serial.println("Estoy EnCurso");   
     } else if(estadoBuild == EstadoBuildEnum::Exitoso) {
