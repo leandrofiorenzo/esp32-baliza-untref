c511a61076dfb20739e5d2f8ad58f81d3e05f9c4
Lucho20 <muratore2401@gmail.com>
2019-11-13 09:56:52 -0300

    Subida de algunos casos de aceptacion

---
+++ test/ManejadorDelProgramaTest.cpp
@@ -0,0 +1,15 @@
+/*
+  Dado un Esp32 conectado por WiFi a un servidor Travis y con un led verde encendido
+  Cuando hago un push al repositorio y me devuelve que el build es fallido
+  Entonces el Esp32 apaga el led verde y enciende el led rojo
+*/
+/*
+  Dado un Esp32 conectado por WiFi a un servidor Travis y con un led rojo encendido
+  Cuando hago un push al repositorio y me devuelve que el build es correcto
+  Entonces el Esp32 apaga el led rojo y enciende el led verde
+*/
+/*
+  Dado un Esp32 conectado por WiFi a un servidor Travis
+  Cuando se corta la conexion de WiFi
+  Entonces el Esp32 enciende un led Naranja
+*/
