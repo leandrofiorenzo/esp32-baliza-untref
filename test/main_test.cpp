487ce32ac891d88e64ee819f0676b7d11c6830f5
Lucho20 <muratore2401@gmail.com>
2019-11-13 16:47:53 -0300

    Arreglo main_test

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -6,7 +6,7 @@ void test_cambio_estado_exitoso_a_fallido(void) {
     TEST_ASSERT_EQUAL(2, 2);
 }
 
-void test_cambio_estado_fallido_a_exitoso(void) {
+/*void test_cambio_estado_fallido_a_exitoso(void) {
     TEST_ASSERT_EQUAL(2, 2);
 }
 
@@ -16,14 +16,14 @@ void test_procesamiento_estado_repositorio(void) {
 
 void test_aviso_perdida_conexion(void) {
     TEST_ASSERT_EQUAL(2, 2);
-}
+}*/
 
 int main(int argc, char **argv) {
     UNITY_BEGIN();
     RUN_TEST(test_cambio_estado_exitoso_a_fallido);
-    RUN_TEST(test_cambio_estado_fallido_a_exitoso);
+    /*RUN_TEST(test_cambio_estado_fallido_a_exitoso);
     RUN_TEST(test_procesamiento_estado_repositorio);
-    RUN_TEST(test_aviso_perdida_conexion);
+    RUN_TEST(test_aviso_perdida_conexion);*/
     UNITY_END();
 
     return 0;
