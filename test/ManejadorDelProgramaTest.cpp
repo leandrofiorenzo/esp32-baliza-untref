da7675c97616d731911ecc8a145957fb465ea922
Lucho20 <muratore2401@gmail.com>
2019-11-13 16:30:28 -0300

    Se agregan nuevos test

--- test/ManejadorDelProgramaTest.cpp
+++ test/ManejadorDelProgramaTest.cpp
@@ -9,7 +9,12 @@
   Entonces el Esp32 apaga el led rojo y enciende el led verde
 */
 /*
-  Dado un Esp32 conectado por WiFi a un servidor Travis
+  Dado un Esp32 conectado por WiFi a un servidor Travis y cualquier led encendido (sea verde o rojo)
   Cuando se corta la conexion de WiFi
-  Entonces el Esp32 enciende un led Naranja
+  Entonces el Esp32 enciende un led Naranja y apaga el led encendido (sea verde o rojo)
+*/
+/*
+  Dado un Esp32 conectado por WiFi a un servidor Travis
+  Cuando hago un push al repositorio y se encuentra validando el estado actual del build
+  Entonces el Esp32 encendera un led amarillo mientras realiza el procesamiento
 */
