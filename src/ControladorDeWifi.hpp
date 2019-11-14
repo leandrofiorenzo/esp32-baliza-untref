3a67f1f858cb636dcae711074ccc6903f863378a
Lucho20 <muratore2401@gmail.com>
2019-11-14 15:38:36 -0300

    Se agrega metodo CambiarCredencialesConexion

--- src/ControladorDeWifi.hpp
+++ src/ControladorDeWifi.hpp
@@ -1,9 +1,13 @@
 #include <WiFi.h>
 
 class ControladorDeWifi {
+    private:
+        const char* nombreRed = ""; 
+        const char* contraseñaRed = "";
     public:
         ControladorDeWifi();
         void establecerConexionWiFi(const char *ssid, const char *passphrase);      
         boolean estaConectado();
         const char* obtenerEstadoDeLaConexion(wl_status_t status);
+        //void cambiarCredencialesConexion(const char *ssid, const char *passphrase);
 };
