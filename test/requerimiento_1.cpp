f387e89959794b0ad23a2eb76df88251035b41c3
Lucho20 <muratore2401@gmail.com>
2019-11-14 08:27:43 -0300

    Se agregan un par de detalles al test

--- test/requerimiento_1.cpp
+++ test/requerimiento_1.cpp
@@ -2,28 +2,28 @@
 
 /* Dado un ESP32 conectado por WiFi a un servidor de Integración Continua con el último build en estado exitoso 
 Cuando haya un push al repositorio y el estado del build sea exitoso 
-Entonces el ESP32 debe permanecer en estado exitoso */
+Entonces el ESP32 debe permanecer en estado exitoso (led verde)*/
 void testUltimoBuildExitosoConPushExitosoNoCambiaEstado(void) {
     TEST_ASSERT_EQUAL(2, 2);
 }
 
 /*Dado un ESP32 conectado por WiFi a un servidor de Integración Continua con el último build en estado fallido 
 Cuando haya un push al repositorio y el estado del build sea fallido 
-Entonces el ESP32 debe permanecer en estado fallido*/
+Entonces el ESP32 debe permanecer en estado fallido (led rojo)*/
 void testUltimoBuildFallidoConPushFallidoNoCambiaEstado(void) {
     TEST_ASSERT_EQUAL(2, 2);
 }
 
 /*Dado un ESP32 conectado por WiFi a un servidor de Integración Continua con el último build en estado exitoso 
 Cuando haya un push al repositorio y el estado del build sea fallido 
-Entonces el ESP32 debe cambiar el estado a fallido*/
+Entonces el ESP32 debe cambiar el estado a fallido (debe encender el led rojo y apagar los demas)*/
 void testUltimoBuildFallidoConPushExitosoCambiaEstadoAExitoso(void) {
     TEST_ASSERT_EQUAL(2, 2);
 }
 
 /*Dado un ESP32 conectado por WiFi a un servidor de Integración Continua con el último build en estado fallido 
 Cuando haya un push al repositorio y el estado del build sea exitoso 
-Entonces el ESP32 debe cambiar el estado a exitoso*/
+Entonces el ESP32 debe cambiar el estado a exitoso (debe encender el led verde y apagar los demas)*/
 void testUltimoBuildExitosoConPushFallidoCambiaEstadoAFallido(void) {
     TEST_ASSERT_EQUAL(2, 2);
 }
