541b1cd49f63c0f01f3595e834e146f34fa921bf
Leandro Fiorenzo <lfiorenzo@acudir.net>
2019-11-21 19:08:50 -0300

    Romper test.

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,6 @@
 
 ManejadorDelPrograma manejadorDelPrograma;
 
-#include "BluetoothSerial.h"
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
-
-BluetoothSerial SerialBT;
-
 void setup () {
     Serial.begin(115200);
     const char* ssid = "Fibertel WiFi NUMERO 2";
