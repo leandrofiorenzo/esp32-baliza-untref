fd61eed63fe1cf4bd4bacc78f9125d34fcbdbef1
Leandro Fiorenzo <lfiorenzo@acudir.net>
2019-11-21 19:04:27 -0300

    Ponerlo

--- src/main.cpp
+++ src/main.cpp
@@ -8,32 +8,47 @@
 ManejadorDelPrograma manejadorDelPrograma;
 
 #include "BluetoothSerial.h"
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
 
 void setup () {
     Serial.begin(115200);
+    SerialBT.begin("ESP32test"); //Bluetooth device name
+    Serial.println("The device started, now you can pair it with bluetooth!");
+
     //const char* ssid = "Fibertel WiFi NUMERO 2";
     //const char* passphrase = "00416040571";
     //const char* ssid = "WiFiSTI";
     //const char* passphrase = "solu*1234";
-    const char* ssid = "wifi18878";
-    const char* passphrase = "51883488";
-    manejadorDelPrograma.establecerConexionWiFi(ssid, passphrase);
+    //const char* ssid = "wifi18878";
+    //const char* passphrase = "51883488";
+    //manejadorDelPrograma.establecerConexionWiFi(ssid, passphrase);
 }
 
 void loop () {
-    //TRAVIS  
-    /*TravisStrategy *travisStrategy = new TravisStrategy();
+    if (Serial.available()) {
+        SerialBT.write(Serial.read());
+    }
+    if (SerialBT.available()) {
+        Serial.write(SerialBT.read());
+    }
+    delay(20);
 
-    const char* tokenAccesso = "zxiel_jS6Xaaok3zgnHGzQ";
-    travisStrategy->definirTokenAcceso(tokenAccesso);
-    
-    const char* repositorioId = "26796622";
-    travisStrategy->definirRepositorioId(repositorioId);
-
-    manejadorDelPrograma.definirServidorDeIntegracionContinua(travisStrategy);*/
+    //TravisCI
+    // TravisStrategy *travisStrategy = new TravisStrategy();
+    // const char* tokenAccesso = "zxiel_jS6Xaaok3zgnHGzQ";
+    // travisStrategy->definirTokenAcceso(tokenAccesso); 
+    // const char* repositorioId = "27012148";
+    // travisStrategy->definirRepositorioId(repositorioId);  
+    // manejadorDelPrograma.definirServidorDeIntegracionContinua(travisStrategy);
 
     //CircleCI
-    CircleCIStrategy *circleCIStrategy = new CircleCIStrategy();
-    manejadorDelPrograma.definirServidorDeIntegracionContinua(circleCIStrategy);
-    manejadorDelPrograma.ejecutarRutinaDeVerificacion();
+    //CircleCIStrategy *circleCIStrategy = new CircleCIStrategy();
+    //manejadorDelPrograma.definirServidorDeIntegracionContinua(circleCIStrategy);
+    
+    //ESTO VA SIEMPRE
+    //manejadorDelPrograma.ejecutarRutinaDeVerificacion();
 }
